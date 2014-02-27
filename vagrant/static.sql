3cdd7e53d227a49558e064b8f1f8821eb0abaa41
guy garnier <guy@nilobstat.com>
2014-02-27 15:46:24 +0100

    Ajout du fichier de création de la base de données.
    Signed-off-by: guy garnier <guy@nilobstat.com>

---
+++ vagrant/static.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE nilobstat_wiki CHARACTER SET utf8;
+COLLATE utf8_general_ci
+
+-- Création du compte utilisateur pour l'accès web.
+CREATE USER 'wikiuser'@'localhost' IDENTIFIED BY 'wikiuser';
+GRANT ALL PRIVILEGES ON nilobstat_wiki TO 'wikiuser'@'localhost';
+
+-- Création du compte admin
+CREATE USER 'wikiadmin'@'localhost' IDENTIFIED BY 'wikiadmin';
+GRANT ALL PRIVILEGES ON nilobstat_wiki TO 'wikiadmin'@'localhost';
