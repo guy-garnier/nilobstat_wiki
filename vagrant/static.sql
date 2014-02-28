93c88e9da97ef88e2bfec9e0814acab98a5f294e
guy garnier <guy@nilobstat.com>
2014-02-28 10:43:17 +0100

    Correction du script de création static.sql.
    Signed-off-by: guy garnier <guy@nilobstat.com>

--- vagrant/static.sql
+++ vagrant/static.sql
@@ -1,7 +1,10 @@
 CREATE DATABASE nilobstat_wiki CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+FLUSH PRIVILEGES;
 -- Création du compte utilisateur pour l'accès web.
 CREATE USER 'wikiuser'@'localhost' IDENTIFIED BY 'wikiuser';
+
+USE nilobstat_wiki;
 GRANT ALL PRIVILEGES ON nilobstat_wiki TO 'wikiuser'@'localhost';
 
 -- Création du compte admin
